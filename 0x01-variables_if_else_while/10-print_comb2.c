62c2ece0dfb0edfdae3ee655a911f484dbb27dd5
KatyaKalache <katya@kalache.fr>
2017-02-10 22:47:10 +0000

    Numbers and spaces

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  int j;
+
+  for (i = 0; i <= 9; i++)
+    {
+      for (j = 0; j <= 9; j++)
+	{
+	  putchar(i + '0');
+	  putchar(j + '0');
+
+      if (i == 9 && j == 9)
+	{break;}
+
+      putchar(',');
+      putchar(' ');
+
+}
+    }
+  putchar('\n');
+  return (0);
+}
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  int j;
+
+  for (i = 0; i <= 9; i++)
+    {
+      for (j = 0; j <= 9; j++)
+  {
+
+    if (i == j || i > j)
+      {
+        continue;
+      }
+    putchar(i + '0');
+    putchar(j + '0');
+    if (i == 8)
+      {break;}
+    putchar(',');
+    putchar(' ');
+ 
+}
+
+
+  }
+
+putchar('\n');
+
+return (0);
+}
+
