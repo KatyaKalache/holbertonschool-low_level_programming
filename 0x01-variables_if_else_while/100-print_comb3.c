e548d67d45b33ff59ea4aa586b182152d0676e09
KatyaKalache <katya@kalache.fr>
2017-02-10 23:09:45 +0000

    Betty style edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int i;
-  int j;
+	int i;
+	int j;
 
-  for (i = 0; i <= 9; i++)
-    {
-      for (j = 0; j <= 9; j++)
-  {
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == j || i > j)
+			{
+				continue;
+			}
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8)
+			{break;}
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    if (i == j || i > j)
-      {
-        continue;
-      }
-    putchar(i + '0');
-    putchar(j + '0');
-    if (i == 8)
-      {break;}
-    putchar(',');
-    putchar(' ');
- 
+	putchar('\n');
+	return (0);
 }
-
-
-  }
-
-putchar('\n');
-
-return (0);
-}
-
