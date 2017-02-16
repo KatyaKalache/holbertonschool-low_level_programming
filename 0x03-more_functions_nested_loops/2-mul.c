9b7016be26bfd6908a46fed8b5b74dc775581cd4
KatyaKalache <katya@kalache.fr>
2017-02-16 17:14:36 +0000

    Multiply 2 int

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+#include "holberton.h"
+
+/**
+ * _isupper  - checks for upper case
+ * @c: the value to be cheched
+ * Return: 1 is is upper, 0 id not
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+	return (sum);
+}
