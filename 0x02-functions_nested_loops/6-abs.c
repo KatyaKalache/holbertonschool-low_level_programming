f91234056df1dec7ab191b4eca35422f8d11f1d1
KatyaKalache <katya@kalache.fr>
2017-02-16 03:47:10 +0000

    Signs and abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * int_abs - computes the absolute value
+ * @i - the value to be checked
+ * Return: return result
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	i *= -1;
+		return (i);
+	}
+	else
+	{
+	return (i);
+	}
+}
