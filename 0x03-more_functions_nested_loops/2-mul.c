78e46eaf44c4e8d9ba3067102873d359102d26ae
KatyaKalache <katya@kalache.fr>
2017-02-16 17:17:29 +0000

    Editing betty style

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
-#include "holberton.h"
-
 /**
- * _isupper  - checks for upper case
- * @c: the value to be cheched
- * Return: 1 is is upper, 0 id not
+ * mul  - multiplying 2 int
+ * @a: the first value to be cheched
+ * @b: the second value to be checked
+ * Return: result
  */
 int mul(int a, int b)
 {
 	int sum = a * b;
+
 	return (sum);
 }
