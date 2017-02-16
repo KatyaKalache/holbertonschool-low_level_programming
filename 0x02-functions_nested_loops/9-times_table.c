cca1d44149f2399ebb79de64932bb1acb458e9b3
KatyaKalache <katya@kalache.fr>
2017-02-16 04:08:22 +0000

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,7 +29,7 @@ void times_table(void)
 			{
 				_putchar(x / 10 + '0');
 				_putchar(x % 10 + '0');
-				if(j == 9)
+				if (j == 9)
 				{ break; }
 				_putchar(',');
 				_putchar(' ');
@@ -38,7 +38,7 @@ void times_table(void)
 			{
 				_putchar(x % 10 + '0');
 				_putchar(',');
-                                _putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
