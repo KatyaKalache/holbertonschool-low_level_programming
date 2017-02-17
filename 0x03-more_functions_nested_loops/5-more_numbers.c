9b18c9cfb72ee9e9431e9c7f282f449d18f63ac7
KatyaKalache <katya@kalache.fr>
2017-02-17 06:42:52 +0000

    Looping 10 times

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -8,6 +8,10 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
 
 	for (i = 0; i <= 14; i++)
 	{
@@ -15,4 +19,5 @@ void more_numbers(void)
 		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
+	}
 }
