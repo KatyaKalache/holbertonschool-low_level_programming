69eebae4443c3843eb5b4a3ef6618c0bd1c82da7
KatyaKalache <katya@kalache.fr>
2017-02-17 06:38:36 +0000

    Editing betty

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
 
+/**
+ * more_numbers - prints numbers from 0 to 14
+ *
+ * Return: nothing
+ */
 void more_numbers(void)
 {
 	int i;
 
-		for(i = 0; i <= 14; i++)
-		{
+	for (i = 0; i <= 14; i++)
+	{
 		_putchar(i / 10 + '0');
 		_putchar(i % 10 + '0');
-		}
+	}
 	_putchar('\n');
 }
