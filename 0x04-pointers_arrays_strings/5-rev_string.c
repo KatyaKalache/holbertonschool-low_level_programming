29d4d46d394a23f091665a3b6c60f4c444003231
KatyaKalache <katya@kalache.fr>
2017-02-22 23:48:52 +0000

    Reversing holberton

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -5,9 +5,31 @@
  * @s: value to be checked
  * Return: length
  */
+	int _strlen(char *s)
+	{
+		int l = 0;
+
+		while (*(s + l))
+			l++;
+		return (l);
+	}
+/**
+ * rev_string - reversing the string
+ * @s: string value
+ * Return: nothing
+ */
 void rev_string(char *s)
 {
-	while (*s)
-		_putchar(*s);
-	s++;
+	int i = 0;
+	int l = _strlen(s) - 1;
+	int temp;
+
+	while (i < l)
+	{
+		temp = s[i];
+		s[i] = s[l];
+		s[l] = temp;
+	i++;
+	l--;
+	}
 }
