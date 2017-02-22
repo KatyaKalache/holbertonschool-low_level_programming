7d8c4a5846ca7402e24ca1588297214ce36f2996
KatyaKalache <katya@kalache.fr>
2017-02-22 03:06:14 +0000

    Moving description for 2nd function right above the 2nf function code block

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,10 @@
 #include "holberton.h"
 
 /**
- * puts_half - returns length of a string
  * _strlen - gets the length
- * @str: value to be checked
  * @s:checking the length
  * Return: length
  */
-
 int _strlen(char *s)
 {
 	int l = 0;
@@ -16,7 +13,11 @@ int _strlen(char *s)
 		l++;
 	return (l);
 }
-
+/**
+ * puts_half - returns length of a string
+ * @str: value to be checked
+ * Return: half length
+ */
 void puts_half(char *str)
 {
 	int i = _strlen(str);
