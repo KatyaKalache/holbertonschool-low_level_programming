4acdb3fa76a6f91d99a42384c67aeb529c98f145
KatyaKalache <katya@kalache.fr>
2017-02-23 22:12:21 +0000

    Adding src to dest

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
