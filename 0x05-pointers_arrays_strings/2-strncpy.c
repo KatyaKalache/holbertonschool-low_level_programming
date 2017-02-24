5a027093cb7bc5a4c7f6dccc08c6c7248833f17f
KatyaKalache <katya@kalache.fr>
2017-02-24 23:52:49 +0000

    Editing betty

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
 	int j = 0;
 
 	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[j] = src[j];
 		j++;
 	}
-
-	while (j < n )
+	while (j < n)
 	{
 		dest[j] = '\0';
 		j++;
-}
+	}
 	return (dest);
 }
