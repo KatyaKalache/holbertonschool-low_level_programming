946a58cddbfc1aa2af4997b17dc9e29213e46aee
KatyaKalache <katya@kalache.fr>
2017-02-24 03:37:10 +0000

    String compare

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - adding two lines together
+ * @s1: first string
+ * @s2: 2nd string
+ * Return: nothing
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int result;
+
+	if (s1[i] != s2[i])
+	{
+		result = (s1[i]) - (s2[i]);
+		i++;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
