2d543cf409eb04d344af5368aa4a7210e0670151
KatyaKalache <katya@kalache.fr>
2017-02-25 02:49:13 +0000

    Changing to array solution

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,26 @@
 #include "holberton.h"
 
 /**
- * cap_string - check the code for Holberton School students.
- * @p: checking values
+ * cap_string - replacing lowcase after symbols w/ caps
+ * @s: checking values
  * Return: result
  */
-char *cap_string(char *p)
+char *cap_string(char *s)
 {
-		int i = 0;
+	int i = 0;
+	int j;
+	char a[] = {' ', '\t', '\n', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-		while (p[i] != '\0')
+	while (s[i] != '\0')
+	{
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			switch (p[i])
+			if ((s[i] == a[j]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			{
-			case ' ':
-			case '\t':
-			case '\n':
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			(p[i + 1] = p[i] - 32);
+				s[i + 1] = s[i + 1] -  32;
 			}
-			i++;
 		}
-		return (p);
+		i++;
+	}
+	return (s);
 }
