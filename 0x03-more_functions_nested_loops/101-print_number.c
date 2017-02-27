f4eefe53725139c6e80e343d4548a0d372a9ab6f
KatyaKalache <katya@kalache.fr>
2017-02-27 05:27:19 +0000

    Cant call function within itself

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,14 @@
  */
 void print_number(int n)
 {
-	while (n >= 0)
+	if (n > 0)
 	{
-		if (n / 10)
-		print_number(n / 10);
-		{
-			_putchar((n % 10) + '0');
-		}
-		break;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n == 0)
+	{
+		_putchar('0');
 	}
 	if (n < 0)
 	{
