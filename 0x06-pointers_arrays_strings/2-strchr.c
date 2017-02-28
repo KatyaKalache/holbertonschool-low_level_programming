7864b3d4d19f9a00908cc5b268e6a2a5b1c04f82
KatyaKalache <katya@kalache.fr>
2017-02-28 00:49:36 +0000

    Checking for chars in the string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: the string
+ * @c: character to be located
+ * Return: pointers to first occurrence or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		if (s == '\0')
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
