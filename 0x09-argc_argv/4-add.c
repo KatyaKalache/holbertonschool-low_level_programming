e6c577bc666abde9e08f195c7018d4917d55dc97
KatyaKalache <katya@kalache.fr>
2017-03-04 05:08:59 +0000

    Multiplication of arguments

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print sum of arguments received in command line
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: sum of arguments
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
