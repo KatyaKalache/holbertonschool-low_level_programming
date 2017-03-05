57acd6ce7db84bcd422b983a35d74b2cb3120290
KatyaKalache <katya@kalache.fr>
2017-03-05 22:51:52 +0000

    Adding description for argv

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -4,6 +4,7 @@
 /**
  * main - returns number of commands entered
  * @argc: number of command entered
+ * @argv: arguments entered
  * Return: number of commands in prompt
  */
 int main(int argc, __attribute__((unused))char *argv[])
