903195f75774708701d94fb85f14662367b19452
KatyaKalache <katya@kalache.fr>
2017-03-09 03:04:40 +0000

    Creating file for #4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free the grid
+ * @grid: to free
+ * @height: size to check
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+
+}
