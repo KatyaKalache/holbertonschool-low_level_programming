62bf2459fa8dffe58e5f1925bb70f8c38830508a
KatyaKalache <katya@kalache.fr>
2017-03-09 22:32:01 +0000

    betty happy

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -40,9 +40,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	lens1 = _strlen(s1);
 	lens2 = _strlen(s2);
 	if (lens2 > n)
-        {
-                lens2 = n;
-        }
+	{
+		lens2 = n;
+	}
 
 	fulllength = malloc((lens1 + lens2) * sizeof(char) + 1);
 	for (i = 0; i < lens1; i++)
