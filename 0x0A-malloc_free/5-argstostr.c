291db111dd3d94b137ecad4458f7c1af709a1547
KatyaKalache <katya@kalache.fr>
2017-03-09 05:32:46 +0000

    Creating file for 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments
+ * @ac: argument count
+ * @av: arguments passed
+ * Return: pointer to a string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+}
