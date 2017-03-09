901ce9ceccd9e6d978fd9a840a6c7042bb048fd5
KatyaKalache <katya@kalache.fr>
2017-03-09 20:22:57 +0000

    Adding REadme and dwscription

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * malloc_checked - chekcing if enough space for passed b
+ * @b: value checked
+ * Return: pointer
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *pointer;
