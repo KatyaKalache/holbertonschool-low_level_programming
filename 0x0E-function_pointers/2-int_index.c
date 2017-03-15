abec9010a8162ed99045a72d184e793480910712
KatyaKalache <katya@kalache.fr>
2017-03-15 23:04:48 +0000

    Array

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - searches for integer
+ * @array: array to be checked
+ * @size: size of the array
+ * @cmp: pointer to a function that compares values
+ * Return: result
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL)
+		return (0);
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			return (i);
+		}
+	}
+		return (-1);
+}
