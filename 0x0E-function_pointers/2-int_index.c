aabbea35855d9bdde2276fe73bfcb2e45be047f9
KatyaKalache <katya@kalache.fr>
2017-03-15 23:25:33 +0000

    size conditi

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,8 +10,11 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	if (size != 0 && cmp != NULL && array != NULL)
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp != NULL && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
