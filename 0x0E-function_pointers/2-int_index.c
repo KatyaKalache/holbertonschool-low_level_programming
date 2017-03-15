ac4fe47254086af92abe8a71beb049c5aaec6c9e
KatyaKalache <katya@kalache.fr>
2017-03-15 23:33:12 +0000

    if stat out

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,17 +10,19 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (size <= 0)
 	{
 		return (-1);
 	}
-	if (cmp != NULL && array != NULL)
+	if (cmp == NULL || array == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
