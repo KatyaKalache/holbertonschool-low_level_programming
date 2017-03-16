3604b4e3a163970a4f56f164d894ec050e503d3b
KatyaKalache <katya@kalache.fr>
2017-03-16 04:38:56 +0000

    For n3

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+/**
+ * op_add - sums up
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+/**
+ * op_sub - minus
+ * @a: 1st N
+ * @b: 2nd N
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	int sub;
+
+	sub = a - b;
+	return (sub);
+}
+/**
+ * op_mul - multiplies
+ * @a: 1st n
+ * @b: 2nd n
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+/**
+ * op_div - devides
+ * @a: 1st
+ * @b: 2nd
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	int div;
+
+	div = a / b;
+	return (div);
+}
+/**
+ * op_mod - get remainder
+ * @a: 1st n
+ * @b: 2nd n
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	int mod;
+
+	mod = a % b;
+	return (mod);
+}
