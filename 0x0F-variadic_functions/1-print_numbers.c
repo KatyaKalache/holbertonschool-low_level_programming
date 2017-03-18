1122442060ae1f6e741598d3909f9d2db4d99364
KatyaKalache <katya@kalache.fr>
2017-03-18 20:30:03 +0000

    changing if statement of separator to n-1

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -13,15 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(param, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(param, int));
-		if (separator != NULL)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
 }
-	printf("%d", va_arg(param, int));
 	printf("\n");
 	va_end(param);
 }
