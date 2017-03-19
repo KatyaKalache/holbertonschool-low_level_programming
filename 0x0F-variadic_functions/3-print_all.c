43cd5b3c9fa04d800f2e7c5dcaa665a815cf0c08
KatyaKalache <katya@kalache.fr>
2017-03-19 20:59:24 +0000

    rm format-1

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -40,7 +40,7 @@ void print_all(const char * const format, ...)
 		default:
 			break;
 		}
-		if (format[i - 1] && format[i + 1] && success == 1)
+		if (format[i + 1] && success == 1)
 		{
 			printf(", ");
 		}
