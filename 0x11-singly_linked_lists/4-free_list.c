9f19fdcf7cb4b88b7deb14ff400d440222baf153
KatyaKalache <katya@kalache.fr>
2017-03-24 05:54:58 +0000

    initial commit of ex 4

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_list - frees the list
+ * @head: firat element of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tempnode;
+
+	tempnode = malloc(sizeof(list_t));
+	if (head != NULL)
+	tempnode = head;
+	head = tempnode;
+	free(tempnode);
+}
