72db426fec9ee7ec5e3c447113345451b100712f
KatyaKalache <katya@kalache.fr>
2017-03-24 06:13:34 +0000

    cleared a bit more space

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * free_list - frees the list
  * @head: firat element of the list
@@ -10,11 +9,10 @@
 void free_list(list_t *head)
 {
 	list_t *tempnode;
-
-	tempnode = malloc(sizeof(list_t));
-	if (head != NULL)
-	tempnode = head;
+	while(head)
+	{
+	tempnode = head->next;
+	free(head);
 	head = tempnode;
-	free(tempnode);
-	head = NULL;
+	}
 }
