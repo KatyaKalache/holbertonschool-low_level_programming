9ad419b87d79ab37d70ddb7a26646642ac9763c5
KatyaKalache <katya@kalache.fr>
2017-03-28 22:51:36 +0000

    initial commit

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes a node at nth index
+ * @head: pointer to the list
+ * @index: nth position
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tempnode1;
+	listint_t *tempnode2;
+	unsigned int counter;
+
+	counter = 0;
+	tempnode1 = *head;
+	if (index == 0)
+	{
+		*head = tempnode1->next;
+		free(tempnode1);
+		return (1);
+	}
+	if (*head != NULL)
+	{
+	for (tempnode1 = *head; tempnode1 != NULL; tempnode1 = tempnode1->next)
+	{
+
+		if (counter == (index - 1))
+		{
+			tempnode2 = tempnode1->next;
+			tempnode1->next = tempnode2->next;
+		}
+		counter++;
+	}
+	free(tempnode2);
+	return (1);
+	}
+	else
+		return (-1);
+}
