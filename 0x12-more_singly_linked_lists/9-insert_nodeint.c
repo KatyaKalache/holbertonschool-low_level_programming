2faa514e7ffa1f80023e60ac6c94ef85721e9ec7
KatyaKalache <katya@kalache.fr>
2017-03-28 22:49:59 +0000

    updated 5 file

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,5 +29,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->next = tempnode->next;
 	tempnode->next = newnode;
-	return (*&newnode);
+	return (newnode);
 }
