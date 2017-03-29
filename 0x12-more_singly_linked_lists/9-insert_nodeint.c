f34b4ef1b6c14270cf89c4730a0201870014a1f5
KatyaKalache <katya@kalache.fr>
2017-03-29 06:07:40 +0000

    novian while and if statements out of main loop

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,21 +16,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	tempnode = *head;
 	counter = 0;
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	while (tempnode && (counter < idx - 1))
+	{
+		tempnode = tempnode->next;
+		counter++;
+	}
 	if (tempnode || (counter == idx - 1) || idx == 0)
 	{
 		newnode = malloc(sizeof(listint_t));
 		if (newnode)
 		{
 			newnode->n = n;
-			if (*head == NULL)
-			{
-				return (NULL);
-			}
-			while (tempnode && (counter < idx - 1))
-			{
-				tempnode = tempnode->next;
-				counter++;
-			}
 			if (idx == 0)
 			{
 				newnode->next = *head;
