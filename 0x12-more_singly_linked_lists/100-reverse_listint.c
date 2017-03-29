870de2fa7210e8284f45bf07411ccdfff8c1e865
KatyaKalache <katya@kalache.fr>
2017-03-29 06:19:37 +0000

    initial commit(files) & updated lists.h

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a list
+ * @head: pointer to list
+ * Return: pointer to the first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
