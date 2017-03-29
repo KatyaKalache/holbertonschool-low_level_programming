dc0fcb5a07753cc488d1977a9d2da26ea64bf7e5
KatyaKalache <katya@kalache.fr>
2017-03-29 20:04:19 +0000

    first code check

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * reverse_listint - reverses a list
  * @head: pointer to list
@@ -6,5 +7,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *tempnode;
+	listint_t *newnode;
 
+	tempnode = NULL;
+
+	while (*head != NULL)
+	{
+		newnode = (*head)->next;
+		(*head)->next = tempnode;
+		tempnode = *head;
+		*head = newnode;
+	}
+	*head = tempnode;
+	return (*head);
 }
