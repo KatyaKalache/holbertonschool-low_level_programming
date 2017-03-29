7ddd904ff6647ce060d75394d8cbbc4ba312fe5b
KatyaKalache <katya@kalache.fr>
2017-03-29 05:34:18 +0000

    if head is null advance the head to next node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,8 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				tempnode = tempnode->next;
 				counter++;
 			}
+			if (idx == 0)
+			{
+				newnode->next = *head;
+				*head = newnode;
+			}
+			else
+			{
 			newnode->next = tempnode->next;
 			tempnode->next = newnode;
+			}
 		}
 		return (newnode);
 	}
