e917606938b1ca4b1a5bba8de8853b88bea71df4
KatyaKalache <katya@kalache.fr>
2017-04-01 05:40:56 +0000

    initial commit of 3-main.c

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - prints the result of math operations
+ * @num1: first number passed
+ * @num2: second number passed
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	result = (*get_op_func(argv[1]))(atoi(argv[2]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
+}
