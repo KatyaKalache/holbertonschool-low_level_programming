3545e45f228cb24fba01fe141f2d8bfc3433f190
KatyaKalache <katya@kalache.fr>
2017-04-05 05:15:50 +0000

    removing if filename exists case

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -42,14 +42,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	length = _strlen(text_content);
 	write(fd, text_content, length);
-	if (filename)
-	{
-		if (text_content == NULL)
-		{
-			return (1);
-		}
-		return(-1);
-	}
 	close(fd);
 	return (1);
 }
