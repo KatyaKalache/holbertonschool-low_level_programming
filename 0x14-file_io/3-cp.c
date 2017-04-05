0a97978522c7fd2dae87fbe83900ddf99cdaf4b6
KatyaKalache <katya@kalache.fr>
2017-04-05 06:49:21 +0000

    adding write

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -5,17 +5,46 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
+ * _strlen - finds string length
+ * @s: a string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
+/**
  * main - copies one file to another
  * @argc: number of arguments passed
  * @argv: arguments
  * Return: 0 for success
  */
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
 	int file_from;
 	int file_to;
+	char *buf[1024];
+	int write_to;
+	int length;
 
+	buf = malloc(sizeof(char) * buf);
 	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		return(-1);
+
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
-	return (0);
+
+	length = _strlen(text_content);
+
+	write_to = write(file_from, text_content, length);
+
+	if (write_to == -1)
+		return (-1);
+	close(file_from);
+	return (1);
 }
