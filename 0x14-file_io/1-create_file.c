7ee43875e403824062c8b1f1ab7eccd28bea1405
KatyaKalache <katya@kalache.fr>
2017-04-05 02:44:08 +0000

    removing EXCL and moving fd = .. up

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,19 +20,24 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT);
-	}
-	fd = open(filename, O_EXCL | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR, S_IWUSR);
+
 	if (fd == -1)
 	{
 		return (-1);
 	}
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+
 	write_to_file = write(fd, text_content, 8);
 
 	if (write_to_file == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 
