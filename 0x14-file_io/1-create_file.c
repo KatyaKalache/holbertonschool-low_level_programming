6a3136a59aec99e88d64bddb09c64ad011606e94
KatyaKalache <katya@kalache.fr>
2017-04-05 03:07:08 +0000

    removing is not == length

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -52,11 +52,6 @@ int create_file(const char *filename, char *text_content)
 
 	write_to_file = write(fd, text_content, length);
 
-	if (write_to_file != length)
-	{
-		close(fd);
-		return (-1);
-	}
 	if (write_to_file == -1)
 	{
 		return (-1);
