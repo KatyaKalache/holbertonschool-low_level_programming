da5c52c1c0758de965cbd6ab130bd49d7dc23db7
KatyaKalache <katya@kalache.fr>
2017-04-05 03:08:25 +0000

    adding closefd -1 back

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -52,7 +52,7 @@ int create_file(const char *filename, char *text_content)
 
 	write_to_file = write(fd, text_content, length);
 
-	if (write_to_file == -1)
+	if (write_to_file == -1 || close(fd) == -1)
 	{
 		return (-1);
 	}
