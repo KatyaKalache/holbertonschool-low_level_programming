998a1060dcad0caa560cd0835cfef5425b159208
KatyaKalache <katya@kalache.fr>
2017-04-06 17:23:59 +0000

    Adding error functions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,36 +22,81 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	file_exists(argv[1], argv[2]);
 /* open file from */
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		exit(98);
+		read_error(argv[1]);
 /* open file to */
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, S_IRUSR
 		       | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
-		exit(98);
+		read_error(argv[2]);
 /* read file_from to buffer */
 	file_read = read(file_from, buf, BUFFSIZE);
 	if (file_read == -1)
-		exit(98);
-
+		read_error(argv[1]);
 /* write from buffer to file_to. file_read(size_t) - the n of bytes read  */
 	while (file_read > 0)
 	{
-
 		write_to = write(file_to, buf, file_read);
-
 		if (write_to == -1)
-			exit(99);
+			write_error(argv[2]);
 		file_read = read(file_from, buf, BUFFSIZE);
 		if (file_read == -1)
-			exit(98);
+			read_error(argv[1]);
 	}
 	close(file_from);
+	if (file_from == -1)
+		close_error(file_from);
 	close(file_to);
+	if (file_from == -1)
+		close_error(file_to);
 	return (1);
+}
+/**
+ * read_error - prints error if failed to read file
+ * @file_read: file to be open and read
+ * Return: nothing
+ */
+void read_error(char *file_read)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_read);
+	exit(98);
+}
+/**
+ * write_error - prints error of failed to write to the file
+ * @write_file: file to be written to
+ * Return: nothing
+ */
+void write_error(char *write_file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", write_file);
+	exit(99);
+}
+/**
+ * close_error - prints error message of failed to close fds
+ * @fd: file descriptors to be close
+ * Return: Nothing
+ */
+void close_error(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+/**
+ * argv_exists - checks if files exist
+ * @argv1: first argument passed
+ * @argv2: second argument passed
+ * Return: nothing
+ */
+void file_exists(char *argv1, char *argv2)
+{
+	if (argv1 == NULL)
+		read_error(argv1);
+	if (argv2 == NULL)
+		write_error(argv2);
 }
