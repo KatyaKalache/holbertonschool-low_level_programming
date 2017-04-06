cd1e7f84f1ce980f26023dba88649754377e00d1
KatyaKalache <katya@kalache.fr>
2017-04-06 17:24:44 +0000

    updated with error functions

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -4,4 +4,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
+void read_error(char *file_read);
+void write_error(char *write_file);
+void close_error(int fd);
+void file_exists(char *argv1, char *argv2);
 #endif
