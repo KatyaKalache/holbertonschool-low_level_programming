cc8e10d5166766fceb8846b19f50140561f52f89
KatyaKalache <katya@kalache.fr>
2017-04-06 23:48:39 +0000

    l 35 & l 97 - changing to argv2

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR
 		       | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
-		read_error(argv[1]);
+		read_error(argv[2]);
 	file_read = read(file_from, buf, BUFFSIZE);
 	if (file_read == -1)
 		read_error(argv[1]);
