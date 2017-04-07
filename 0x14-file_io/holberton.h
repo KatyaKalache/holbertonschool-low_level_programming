645a1580404a9bfad80fe7e1c44f33569dab2716
KatyaKalache <katya@kalache.fr>
2017-04-07 00:31:50 +0000

    updater header

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -7,6 +7,6 @@ int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
 void read_error(char *file_read);
 void write_error(char *write_file);
-void close_error(int fd);
+void close_error(char *fd);
 void file_exists(char *argv1, char *argv2);
 #endif
