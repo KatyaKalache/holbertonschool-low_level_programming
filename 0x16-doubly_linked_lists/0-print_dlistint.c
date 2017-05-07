a6c3004bf0cea4cbd1bc897cd484e17b55b9589b
KatyaKalache <katya@kalache.fr>
2017-05-07 00:17:59 +0000

    initial

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+size_t  print_dlistint(const dlistint_t *h)
+{
+	size_t numberElements = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		numberElements++;
+	}
+	return (numberElements);
+}
