f95c5a41c954c25281396105fd9f23d557d6e911
KatyaKalache <katya@kalache.fr>
2017-05-07 08:18:48 +0000

    addig node->prev is null when no head

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,16 +12,17 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 	dlistint_t *temp_node;
 
-	if (!n)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
 		return (NULL);
 	}
+
 	new_node->n = n;
+	if (!n)
+	{
+		return (NULL);
+	}
 	new_node->next = NULL;
 
 	if (*head != NULL)
@@ -32,10 +33,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			temp_node = temp_node->next;
 		}
 		temp_node->next = new_node;
+		new_node->prev = temp_node;
+		new_node->next = NULL;
 	}
 	if (*head == NULL)
 	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
 		*head = new_node;
+		return (new_node);
 	}
 	return (new_node);
 }
