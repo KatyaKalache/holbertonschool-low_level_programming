99b8a5a3b0350f1df95d3bc9a026566883d9f4b0
KatyaKalache <katya@kalache.fr>
2017-05-07 08:28:11 +0000

    rm newnode = null from outside the loop

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,7 +23,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
 	if (*head != NULL)
 	{
@@ -35,13 +42,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			new_node->next = NULL;
 		}
 		temp_node->next = new_node;
-	}
-	if (*head == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
 	}
 	return (new_node);
 }
