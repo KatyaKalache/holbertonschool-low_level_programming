47888ac74dffe71eba21231bcac3fb6cbb2a78a9
KatyaKalache <katya@kalache.fr>
2017-05-08 22:07:08 +0000

    moving temp->next = newnode up

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -42,8 +42,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 /* got to the end of the lost */
 		temp_node = temp_node->next;
 	}
+	temp_node->next = new_node;
 	new_node->next = NULL;
 	new_node->prev = temp_node;
-	temp_node->next = new_node;
 	return (new_node);
 }
