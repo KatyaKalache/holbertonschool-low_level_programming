d8d3c4099b304824d36ca3c30518b68e73488d61
KatyaKalache <katya@kalache.fr>
2017-05-09 01:00:26 +0000

    set newnode->prev to NULL

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -12,22 +12,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	new_node->n = n;
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	if (*head == NULL)
+	if (head != NULL)
 	{
-		*head = new_node;
-		new_node->next = NULL;
+		new_node->n = n;
+		new_node->next = *head;
 		new_node->prev = NULL;
+		*head = new_node;
 		return (new_node);
 		free(new_node);
 	}
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-	free(new_node);
+	else
+		return (NULL);
 }
