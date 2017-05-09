fb27d898123840480853455e64282462e0624852
KatyaKalache <katya@kalache.fr>
2017-05-09 01:32:51 +0000

    removing if no n

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,14 +13,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-	{
-		return (NULL);
-	}
-
 	new_node->n = n;
-	new_node->next = NULL:
-	if (!n)
+
+	if (!new_node)
 	{
 		return (NULL);
 	}
