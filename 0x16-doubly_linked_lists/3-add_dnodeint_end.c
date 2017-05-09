f41dbf0426b2c9a3e0b3cbe81e4e2bcddc1ae082
KatyaKalache <katya@kalache.fr>
2017-05-09 04:31:42 +0000

    newnode->prev = tempnode

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -34,6 +34,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			temp_node = temp_node->next;
 		}
 		temp_node->next = new_node;
+		new_node->prev = temp_node;
 		new_node->next = NULL;
 	}
 	else
