48ed64d41454b0f956f69602ef0b10552c30309f
KatyaKalache <katya@kalache.fr>
2017-06-04 02:42:50 +0000

    rm max check

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
   hash_table_t *new_table = NULL;
   unsigned int i = 0;
 
-  if (size < 1 || size > ULLONG_MAX)
+  if (size < 1)
     return (NULL);
 
   /* allocate table space */
