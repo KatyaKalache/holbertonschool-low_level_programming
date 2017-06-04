447e0fdb40615c99f123c4011fb2fb80c85ccd9d
KatyaKalache <katya@kalache.fr>
2017-06-04 01:45:03 +0000

    initial

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates has table
+ * @size: size of the array
+ * Return: pointer to a new table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+  hash_table_t *new_table;
+  unsigned int i = 0;
+
+
+  new_table = malloc(sizeof(hash_table_t));
+  if (!new_table)
+    return NULL;
+
+  new_table->array = malloc(sizeof(hash_table_t *) * size);
+  if (!new_table->array)
+      return NULL;
+
+  while (i < size)
+    {
+      new_table->array[i] = NULL;
+      i++;
+    }
+  free(new_table->array);
+  free(new_table);
+  return(new_table);
+}
