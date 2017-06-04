f997dafd17746c52e80c013852cfe2f4b4e4507f
KatyaKalache <katya@kalache.fr>
2017-06-04 02:24:35 +0000

    changing type allocated for array pointers

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,13 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
   hash_table_t *new_table;
   unsigned int i = 0;
 
+  if (size < 1 || size > ULLONG_MAX)
+    return (NULL);
+
   /* allocate table space */
   new_table = malloc(sizeof(hash_table_t));
   if (!new_table)
     return (NULL);
 
   /* allocate head node pointers space */ 
-  new_table->array = malloc(sizeof(hash_table_t *) * size);
+  new_table->array = malloc(sizeof(hash_node_t *) * size);
   if (!new_table->array)
     return (NULL);
 
