9f2f9812425a67179d70735881b5ac4c78e96a76
KatyaKalache <katya@kalache.fr>
2017-06-04 02:30:53 +0000

    set new tanle to null

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table;
+  hash_table_t *new_table = NULL;
   unsigned int i = 0;
 
   if (size < 1 || size > ULLONG_MAX)
