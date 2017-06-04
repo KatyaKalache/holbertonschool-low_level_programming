d61f8f123c187533963a2c1101a319b42c5c9625
KatyaKalache <katya@kalache.fr>
2017-06-04 02:54:10 +0000

    initializing below

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -6,8 +6,11 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table = NULL;
-  unsigned int i = 0;
+  hash_table_t *new_table;
+  unsigned int i;
+
+  new_table = NULL;
+  i = 0;
 
   if (size == 0)
     return (NULL);
