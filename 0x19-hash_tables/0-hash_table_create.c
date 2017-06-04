d1659a847a52773b7e2979822e1d6c6cf870764c
KatyaKalache <katya@kalache.fr>
2017-06-04 03:16:44 +0000

    style fix

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -6,27 +6,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned int i;
+	hash_table_t *new_table = NULL;
+	unsigned int i = 0;
 
-	new_table = NULL;
-	i = 0;
 
 	if (size == 0)
+	{
 		return (NULL);
-
+	}
 	/* allocate table space */
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
+	{
 		return (NULL);
+	}
 
 	/* allocate head node pointers space */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
+	{
 		return (NULL);
-
+	}
 	while (i < size)
 	{
 		new_table->array[i] = NULL;
