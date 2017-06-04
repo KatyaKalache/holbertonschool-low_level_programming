cc5a3fba406a6453d5b286660cd96f9b35c88a5a
KatyaKalache <katya@kalache.fr>
2017-06-04 03:12:27 +0000

    betty fix

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -6,31 +6,31 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table;
-  unsigned int i;
+	hash_table_t *new_table;
+	unsigned int i;
 
-  new_table = NULL;
-  i = 0;
+	new_table = NULL;
+	i = 0;
 
-  if (size == 0)
-    return (NULL);
+	if (size == 0)
+		return (NULL);
 
-  /* allocate table space */
-  new_table = malloc(sizeof(hash_table_t));
-  if (new_table == NULL)
-    return (NULL);
+	/* allocate table space */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-  /* allocate head node pointers space */ 
-  new_table->array = malloc(sizeof(hash_node_t *) * size);
-  if (new_table->array == NULL)
-    return (NULL);
+	/* allocate head node pointers space */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+		return (NULL);
 
-  while (i < size)
-    {
-      new_table->array[i] = NULL;
-      i++;
-    }
-  new_table->size = size;
+	while (i < size)
+	{
+		new_table->array[i] = NULL;
+		i++;
+	}
+	new_table->size = size;
 
-  return (new_table);
+	return (new_table);
 }
