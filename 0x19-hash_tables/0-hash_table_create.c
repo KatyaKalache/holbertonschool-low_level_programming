011b0d99bfc3c4aab68dd7f8e44e798c88b10be0
KatyaKalache <katya@kalache.fr>
2017-06-05 18:05:58 +0000

    tabs for comments

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,14 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	/* allocate table space */
+
+/* allocate table space */
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (!new_table)
 	{
 		return (NULL);
 	}
-	/* allocate head node pointers space */
+	
+/* allocate head node pointers space */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!(new_table->array))
