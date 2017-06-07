dbe2f663fbd8abe6d66ded93188eea18f026bcab
KatyaKalache <katya@kalache.fr>
2017-06-07 00:35:07 +0000

    adding another loop to keep track of i so can print commas and spaces

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -11,16 +11,33 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *array_ptr;
 	unsigned long int i = 0;
+	unsigned long int count;
+	hash_node_t *temp;
 
 	if (!ht)
 		return;
+
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			count = i;
+			temp = temp->next;
+		}
+		i++;
+	}
+
+	i = 0;
 	printf("{");
 	while (i < ht->size)
 	{
 		array_ptr = ht->array[i];
 		while (array_ptr)
 		{
-			printf("'%s': '%s', ", array_ptr->key, array_ptr->value);
+			printf("'%s': '%s'", array_ptr->key, array_ptr->value);
+			if (i != count)
+				printf(", ");
 			array_ptr = array_ptr->next;
 		}
 		i++;
