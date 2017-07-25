41f30fb112152594074309feffb1c5849a6d544f
Spencer <spencerhcheng@gmail.com>
2017-07-25 05:28:35 +0000

    1-binary_tree_insert_left: insert node left

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -3,53 +3,27 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
 	
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = value;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	printf("new_node value: %d\n", new_node->n);
-	printf("parent value: %d\n", parent->n);
-	if (parent->left != NULL)
-		printf("parent's left value: %d\n", parent->left->n);
-	printf("-----------------\n");
-
-
-	if (parent != NULL)
-	{
-		if (new_node->n < parent->n)
-		{
-			new_node->parent = parent;
-			parent->left = new_node;
-		}
-		else if (new_node->n > parent->n)
-		{
-			parent->parent = new_node;
-			new_node->parent = parent->parent;
-			new_node->left = parent;
-		}
-	}
-	else
-		parent = new_node;
-		
 	if (parent->left != NULL)
 	{
-		if (new_node->n < parent->left->n)
-		{
-			parent->left = new_node;
-			new_node->parent = parent->parent;
-		}
-		else if (new_node->n > parent->left->n)
-		{
-			printf("Im in here!\n");
-			parent->left->parent = new_node;
-			new_node->parent = parent->left->parent;
-			new_node->left = parent->left;
-		}
+		new_node->left = parent->left;
+		parent->left = new_node;
+		new_node->left->parent = new_node;
 	}
 	else
-		parent = new_node;
+	{
+		parent->left = new_node;
+		new_node = NULL;
+	}
 	return (new_node);
 }
