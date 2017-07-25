cc836ddf385768199bc52d34fd742fd3637588b3
KatyaKalache <katya@kalache.fr>
2017-07-25 06:17:23 +0000

    betty fix

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -8,15 +8,15 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-        new_node = malloc(sizeof(binary_tree_t));
 
+	new_node = malloc(sizeof(binary_tree_t));
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
 	if (parent == NULL)
-		return(NULL);
+		return (NULL);
 
 	if (parent->right)
 	{
