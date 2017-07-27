e9356be787529eb452ac324d536c683c6ac23538
Spencer <spencerhcheng@gmail.com>
2017-07-27 18:14:20 +0000

    16-binary_tree_is_perfect: unstage commits and fix comment

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,53 +1,7 @@
 #include "binary_trees.h"
-<<<<<<< HEAD
-/**
- * binary_tree_is_perfect - checks if binary tree is perfect
- * @tree: tree to check
- * Return: 1 if full, 0 if otherwise
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int left;
-	int right;
-
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	if ((left == right) && (binary_tree_is_full(tree) == 1))
-		return (1);
-	else
-		return (0);
-}
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height of
- * Return: height of the binary tree or 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int count_right = 0;
-	int count_left = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-                return (0);
-	while (tree->left != NULL)
-	{
-		count_left++;
-		tree = tree->left;
-	}
-	while (tree->right != NULL)
-	{
-		count_right++;
-		tree = tree->right;
-	}
-	return (count_right + count_left);
-}
-=======
 
 /**
- * binary_tree_height - checks whether left branch is equal to right branch
+ * height_is_equal - checks whether left branch is equal to right branch
  * @tree: pointer to the root node of the tree to measure the height of
  * Return: 1 if branches are equal, 0 otherwise
  */
@@ -83,7 +37,6 @@ int height_is_equal(const binary_tree_t *tree)
 	}
 }
 
->>>>>>> 52bfad90c03d7b200a326030d85b4de9ed8cbed0
 /**
  * binary_tree_is_full - checks if binary tree is full
  * @tree: pointer to the root node of the tree to count the nodes in
@@ -97,11 +50,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	else
-<<<<<<< HEAD
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
-=======
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	return (0);
 }
 
@@ -121,5 +71,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	else
 		return (0);
->>>>>>> 52bfad90c03d7b200a326030d85b4de9ed8cbed0
 }
