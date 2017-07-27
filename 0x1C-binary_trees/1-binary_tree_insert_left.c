7934b058947a7fdfe9cb486d098e04873b97756f
KatyaKalache <katya@kalache.fr>
2017-07-27 22:35:33 +0000

    moving parent not null check above malloc

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -10,6 +10,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
@@ -19,9 +22,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
-	if (parent == NULL)
-		return (NULL);
 
 	if (parent->left)
 	{
