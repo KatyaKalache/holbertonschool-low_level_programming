a8a1fff677cc3e0016d7c4da26916a3ee1bcde1f
KatyaKalache <katya@kalache.fr>
2017-07-27 19:37:42 +0000

    adding if both trees are null condition

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -49,6 +49,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	if (left == right && binary_tree_is_full(tree) == 1)
 		return (1);
 	else
