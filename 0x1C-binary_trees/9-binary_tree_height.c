8599571edf4b4ecbed7fbe847943a6d2a93e91e0
KatyaKalache <katya@kalache.fr>
2017-07-27 15:11:11 +0000

    rm tree->left or right == null condition

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t count_right = 0, count_left = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
 
