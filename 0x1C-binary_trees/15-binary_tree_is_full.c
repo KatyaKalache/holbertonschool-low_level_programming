36728f9a9a7b8bbf872a3abfde7db6b060d7d8b0
KatyaKalache <katya@kalache.fr>
2017-07-27 18:01:04 +0000

    trying dif edge cases

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -13,6 +13,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	else
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-	return (0);
+		return (binary_tree_is_full(tree->left)
+			&& binary_tree_is_full(tree->right));
 }
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -12,7 +12,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-
 	else
 	{
 		while (tree->left != NULL)
