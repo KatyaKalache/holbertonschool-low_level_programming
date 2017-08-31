f9854e28115569806445b7541ed046dd6308306a
KatyaKalache <katya@kalache.fr>
2017-08-31 21:36:31 +0000

    if array is not null confition added

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	while (i < size)
+	while (i < size && array != NULL)
 	{
 		printf("Value checked array [%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
