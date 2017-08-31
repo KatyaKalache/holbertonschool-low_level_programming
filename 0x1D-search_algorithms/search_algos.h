cc39f7a6a95f3fd5c42025086d1a2d8e0a55b640
KatyaKalache <katya@kalache.fr>
2017-08-31 21:38:04 +0000

    updated header with size_t defined

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,5 +1,6 @@
 #ifndef ALGO_H
 #define ALGO_H
+typedef unsigned long int size_t;
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-#endif ALGO_H
+#endif
